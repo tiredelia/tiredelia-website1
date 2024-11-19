cacc6aa6a04c3a02dfceda2f0cb57ff14478b4aa
Elly <eliadeleon5@gmail.com>
2024-11-19 13:43:58 +0000

    i dont remember

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
     navbg.classList.toggle('active');
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
